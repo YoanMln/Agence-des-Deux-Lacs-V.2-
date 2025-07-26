e282a4bd5d7837b78f27d9b2e78e201ab4d7322c
YoanMln <yoan.moulin@hotmail.fr>
2025-07-26 11:41:47 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# 🏡 Agence des Deux Lacs -V.2- version Laravel
+
+Bienvenue sur la nouvelle version du site **Agence des Deux Lacs**, un projet fictif de site vitrine d’agence immobilière située en Savoie et Haute-Savoie.
+
+Ce projet a été reconstruit avec **Laravel** afin d’explorer une architecture MVC, une logique back-end et poser les bases d’un futur back office.
+
+---
+
+## ✨ Aperçu du projet
+
+Le site présente les principales fonctionnalités d’une agence immobilière :
+
+-   Présentation des services (vente, estimation, gestion locative)
+-   Interface en **one page** avec ancres pour navigation fluide
+-   Design responsive adapté aux différents écrans
+-   Intégration future d’un back office pour la gestion des contenus
+
+---
+
+## ⚙️ Stack & Technologies utilisées
+
+-   **Laravel** — Framework PHP MVC
+-   **Blade** — Moteur de templates Laravel
+-   **SCSS** — Pour une gestion plus propre des styles
+-   **HTML5 / CSS3** — Sémantique et personnalisation
+-   **Responsive Design** — Layouts adaptatifs via media queries
+-   **FontAwesome** — Pour les icônes
+
+---
+
+## 🔐 Fonctionnalités à venir
+
+-   Interface d’administration sécurisée (authentification)
+-   Système CRUD pour la gestion des biens immobiliers
+-   Intégration d'une API météo pour afficher la météo locale
+-   Formulaire de contact avec validation et envoi de mail
+
+---
+
+## 📬 Me contacter
+
+Tu peux me joindre via :
+👉 yoan.moulin@le-campus-numerique.fr
+👉 https://yoanmoulin.dev
+
+---
+
+👉 Retrouvez également la première version du site (HTML/CSS) ici :
+➡️ [https://yoanmln.github.io/Agence-des-Deux-Lacs/](https://yoanmln.github.io/Agence-des-Deux-Lacs/)
---
+++ resources/views/Home.blade.php
@@ -0,0 +1,371 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+        name="description"
+        content="Agence immobilière en Savoie et Haute-Savoie : annonces, estimations et gestion locative." />
+    <title>Agence des deux Lacs</title>
+    <link rel="stylesheet" href="CSS/style.css" />
+    <link rel="stylesheet" href="CSS/fonts.css" />
+    <link rel="stylesheet" href="CSS/responsive.css" />
+    <link rel="stylesheet" href="typography.css" />
+    <script
+        src="https://kit.fontawesome.com/97bb762bba.js"
+        crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="vp-container">
+        <section id="home" class="vp">
+            <div class="vp-content">
+                <a href="#home" class="vp-logo">
+                    <img
+                        class="logo-img"
+                        src="public/images/logo.png"
+                        alt="logo du site Agence des Deux Lacs" />
+
+                    <h1 class="name">Agence des Deux Lacs</h1>
+                </a>
+                <nav class="menu">
+                    <ul>
+                        <li><a href="#offer">Nos dernières offres</a></li>
+                        <li><a href="#news">Actualités</a></li>
+                        <li><a href="#our-selection">Notre sélection</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
+        <!-------Info------->
+        <section class="info">
+            <div class="container-info">
+                <div class="find">
+                    <div class="img-find">
+                        <img
+                            class="logo-find"
+                            src="public/images/annonces vente.png"
+                            alt="logo de la section trouvez un appartement" />
+                    </div>
+                    <h3 class="text-title">Trouvez un appartement</h3>
+                    <p class="text-offer">
+                        Vous recherchez un bien à la vente ou en location en Savoie ou
+                        Haute-Savoie? Consultez nos annonces, elles sont mises à jour
+                        régulierement.
+                    </p>
+                    <div class="offer-container-btn">
+                        <a class="buy-btn" href="#">acheter</a>
+                        <a class="rent-btn" href="#">louer</a>
+                    </div>
+                </div>
+                <div class="estimate">
+                    <div class="img-estimate">
+                        <img
+                            class="logo-estimate"
+                            src="public/images/annonces location.png"
+                            alt="logo de la section estimer" />
+                    </div>
+                    <h3 class="text-title">estimer</h3>
+                    <p class="text-offer">
+                        Vous souhaitez vendre ou louer votre appartement ? à l'agence des
+                        deux lacs, l'estimation est offerte, et établie sous 24h.
+                        Profitez-en !
+                    </p>
+                    <div class="estimate-container-btn">
+                        <a class="estimate-btn" href="#estimer">estimer</a>
+                    </div>
+                </div>
+                <div class="rental-management">
+                    <div class="img-rental">
+                        <img
+                            class="logo-rental"
+                            src="public/images/gestion locative.png"
+                            alt="logo de la section gestion" />
+                    </div>
+                    <h3 class="text-title">faire gérer</h3>
+                    <p class="text-offer">
+                        Vous avez un bien en location et vous hésitez à en confier la
+                        gestion ? Découvrez ce que peut vous apporter notre service de
+                        gestion.
+                    </p>
+                    <div class="more-info-container-btn">
+                        <a class="more-info" href="#">en savoir plus</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-------Offer------->
+        <section id="offer" class="offer">
+            <div class="container">
+                <h2>
+                    <a class="title-last-offers" href="#home">Nos dernières offres</a>
+                </h2>
+                <div class="separator-section">
+                    <span></span>
+                    <i class="fa-solid fa-chevron-down" style="color: #aaaaaa"></i>
+                    <span></span>
+                </div>
+                <article>
+                    <ul class="offer-list">
+                        <li class="offer-elem">
+                            <img
+                                src="public/images/house-1.jpg"
+                                alt="photo d'un appartement à acheter à Aix-les-Bains"
+                                class="offer-elem-img" />
+                            <div class="offer-elem-text">
+                                <p>Appartement - Aix-les-Bains 73100</p>
+                                <p>4 chambres 150m2</p>
+                                <div class="offer-separator"></div>
+                                <p>1 000 000 €</p>
+                            </div>
+                        </li>
+                        <li class="offer-elem">
+                            <img
+                                src="public/images/house-2.jpg"
+                                alt="photo d'un appartement à acheter à Veyrier-du-Lac"
+                                class="offer-elem-img" />
+                            <div class="offer-elem-text">
+                                <p>Appartement - Veyrier-du-Lac 74290</p>
+                                <p>5 chambres 233m2</p>
+                                <div class="offer-separator"></div>
+                                <p>3 900 000 €</p>
+                            </div>
+                        </li>
+                        <li class="offer-elem">
+                            <img
+                                src="public/images/house-3.jpg"
+                                alt="photo d'un appartement à acheter à Chambéry"
+                                class="offer-elem-img" />
+                            <div class="offer-elem-text">
+                                <p>Appartement - Chambéry 73000</p>
+                                <p>5 chambres 215m2</p>
+                                <div class="offer-separator"></div>
+                                <p>460 000 €</p>
+                            </div>
+                        </li>
+                    </ul>
+                </article>
+            </div>
+        </section>
+        <!-------Estimate------->
+        <h2>
+            <a class="estimate-title" href="#home">Estimation</a>
+        </h2>
+        <div class="separator-section">
+            <span></span>
+            <i class="fa-solid fa-chevron-down" style="color: #aaaaaa"></i>
+            <span></span>
+        </div>
+        <section id="estimer" class="estimer">
+            <div class="estimate-container">
+                <div class="online">
+                    <p class="content-estimate">En ligne</p>
+                    <p class="estimate-offer">
+                        Estimation du prix de vente en ligne de votre bien immobilier.
+                    </p>
+                    <a class="estimate-online" href="#">Estimer mon bien en ligne</a>
+                </div>
+                <div class="agency">
+                    <p class="content-estimate">En agence</p>
+                    <p class="estimate-offer">
+                        Estimation du prix de vente de votre bien immobilier <br />
+                        dans notre agence des Deux Lacs.
+                    </p>
+                    <a class="estimate-agency" href="#">Estimer mon bien en agence</a>
+                </div>
+            </div>
+        </section>
+        <!-------News------->
+        <section id="news" class="news">
+            <h2><a class="news-title" href="#home">Actualités</a></h2>
+            <div class="separator-section">
+                <span></span>
+                <i class="fa-solid fa-chevron-down" style="color: #aaaaaa"></i>
+                <span></span>
+            </div>
+            <div class="container-news">
+                <div>
+                    <div class="img-news-1">
+                        <img
+                            class="news-img"
+                            src="./images/news1.jpg"
+                            alt="photo d'illustration d'un article sur une petite ville en Haute-Savoie" />
+                    </div>
+                    <h4 class="title-news">
+                        Haute-Savoie : quelle est cette petite ville qui séduit les
+                        Annéciens ?
+                    </h4>
+                    <p class="date-news">23 juil. 2024</p>
+                    <p class="news-content">
+                        Cette commune de 5 000 habitants, située à 10 km de la
+                        préfecture...
+                    </p>
+                    <div class="read-more-container">
+                        <a href="#" class="read-more-btn">Lire la suite</a>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="img-news-1">
+                        <img
+                            class="news-img"
+                            src="./images/news2.jpg"
+                            alt="photo d'illustration d'un article sur la gestion locative à Aix-les-Bains" />
+                    </div>
+                    <h4 class="title-news">
+                        Gestion locative Aix-les-Bains : l'investissement locatif
+                    </h4>
+                    <p class="date-news">10 juil 2024</p>
+                    <p class="news-content">
+                        Faire louer son bien est une affaire qui peut parfois s'avérer
+                        assez compliquée...
+                    </p>
+                    <div class="read-more-container">
+                        <a href="#" class="read-more-btn">Lire la suite</a>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="img-news-1">
+                        <img
+                            class="news-img"
+                            src="./images/news3.jpg"
+                            alt="photo d'illustration d'un article sur le marché immobilier à Chambéry" />
+                    </div>
+                    <h4 class="title-news">
+                        Chambéry en 2025 : un marché immobilier dynamique et attractif
+                    </h4>
+                    <p class="date-news">5 mai 2025</p>
+                    <p class="news-content">
+                        Chambéry, ville située au cœur de la Savoie, est réputée pour son
+                        cadre de vie agréable...
+                    </p>
+                    <div class="read-more-container">
+                        <a href="#" class="read-more-btn">Lire la suite</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="our-selection" class="our-selection">
+            <div class="our-selection-container">
+                <h2><a class="selection-title" href="#home">Notre sélection</a></h2>
+                <div class="separator-section">
+                    <span></span>
+                    <i class="fa-solid fa-chevron-down" style="color: #aaaaaa"></i>
+                    <span></span>
+                </div>
+                <!-------Carrousel------->
+                <div class="carrousel-selection">
+                    <article>
+                        <ul class="carrousel-items">
+                            <li class="carrousel-item">
+                                <div class="card">
+                                    <img
+                                        class="carrousel-img"
+                                        src="./images/1.png"
+                                        alt="photo de l'intérieur d'un appartement à acheter sur Aix-les-Bains" />
+                                    <p class="card-title">A vendre appartement</p>
+                                    <p class="card-info">Aix-les-Bains</p>
+                                    <p class="card-price">545 000 €</p>
+                                    <div class="carrousel-container-btn">
+                                        <a href="#" class="card-more-info"> En savoir plus </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="carrousel-item">
+                                <div class="card">
+                                    <img
+                                        class="carrousel-img"
+                                        src="./images/1.png"
+                                        alt="photo de l'intérieur d'un appartement à acheter sur Aix-les-Bains" />
+                                    <p class="card-title">A vendre appartement</p>
+                                    <p class="card-info">Aix-les-Bains</p>
+                                    <p class="card-price">545 000 €</p>
+                                    <div class="carrousel-container-btn">
+                                        <a href="#" class="card-more-info"> En savoir plus </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="carrousel-item">
+                                <div class="card">
+                                    <img
+                                        class="carrousel-img"
+                                        src="./images/1.png"
+                                        alt="photo de l'intérieur d'un appartement à acheter sur Aix-les-Bains" />
+                                    <p class="card-title">A vendre appartement</p>
+                                    <p class="card-info">Aix-les-Bains</p>
+                                    <p class="card-price">545 000 €</p>
+                                    <div class="carrousel-container-btn">
+                                        <a href="#" class="card-more-info"> En savoir plus </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="carrousel-item">
+                                <div class="card">
+                                    <img
+                                        class="carrousel-img"
+                                        src="./images/1.png"
+                                        alt="photo de l'intérieur d'un appartement à acheter sur Aix-les-Bains" />
+                                    <p class="card-title">A vendre appartement</p>
+                                    <p class="card-info">Aix-les-Bains</p>
+                                    <p class="card-price">545 000 €</p>
+                                    <div class="carrousel-container-btn">
+                                        <a href="#" class="card-more-info"> En savoir plus </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="carrousel-item">
+                                <div class="card">
+                                    <img
+                                        class="carrousel-img"
+                                        src="./images/1.png"
+                                        alt="photo de l'intérieur d'un appartement à acheter sur Aix-les-Bains" />
+                                    <p class="card-title">A vendre appartement</p>
+                                    <p class="card-info">Aix-les-Bains</p>
+                                    <p class="card-price">545 000 €</p>
+                                    <div class="carrousel-container-btn">
+                                        <a href="#" class="card-more-info"> En savoir plus </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="carrousel-item">
+                                <div class="card">
+                                    <img
+                                        class="carrousel-img"
+                                        src="./images/1.png"
+                                        alt="photo de l'intérieur d'un appartement à acheter sur Aix-les-Bains" />
+                                    <p class="card-title">A vendre appartement</p>
+                                    <p class="card-info">Aix-les-Bains</p>
+                                    <p class="card-price">545 000 €</p>
+                                    <div class="carrousel-container-btn">
+                                        <a href="#" class="card-more-info"> En savoir plus </a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </article>
+                </div>
+            </div>
+        </section>
+        <!-------Contact------->
+        <section id="contact" class="contact">
+            <form action="#" class="contact-form">
+                <h2 class="title">Nous contacter</h2>
+                <input type="text" placeholder="Nom" name="name" id="name" />
+                <input type="email" placeholder="Email" name="email" id="email" />
+                <input type="text" placeholder="Votre message" id="message" />
+                <div>
+                    <button>envoyer</button>
+                </div>
+            </form>
+        </section>
+        <!-------Foooter------->
+        <footer>
+            <p>Copyright -2024-YoanMln</p>
+        </footer>
+    </div>
+</body>
+
+</html>
